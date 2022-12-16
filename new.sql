0cc960c4f59c523427c423a49ebdd89e38a9c05c
christtiane <christtianec@gmail.com>
2022-12-16 11:08:37 -0500

    ADD ORDER BY DESC

--- new.sql
+++ new.sql
@@ -8,3 +8,8 @@ select id, occurred_at, total_amt_usd
 from orders
 ORDER BY occurred_at
 LIMIT 10
+
+-- ORDER BY DECRESCENTE
+select id, account_id, total_amt_usd
+from orders
+order by account_id DESC, total_amt_usd
