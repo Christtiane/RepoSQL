5c64bc5d3cc28b5a44923fc1bf9e2ccd2b8a4cbc
christtiane <christtianec@gmail.com>
2022-12-16 18:27:56 -0500

    statement

--- new.sql
+++ new.sql
@@ -2,7 +2,6 @@ SELECT *
 FROM orders
 LIMIT 10;
 
-
 -- ORDER BY
 select id, occurred_at, total_amt_usd
 from orders
@@ -13,3 +12,52 @@ LIMIT 10
 select id, account_id, total_amt_usd
 from orders
 order by account_id DESC, total_amt_usd
+
+-- ORDER BY ACRESCENTE
+select id, account_id, total_amt_usd
+from orders
+order by account_id ASC, total_amt_usd
+
+-- WHERE
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
+
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
+-- AS
+SELECT id, account_id, standard_amt_usd/standard_qty AS unit_price
+FROM orders
+LIMIT 10;
+
+SELECT id, account_id,
+   poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
+FROM orders
+LIMIT 10;
+
+--LIKE
+SELECT name
+FROM accounts
+WHERE name LIKE 'C%';
+
+-- IN
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+--NOT
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%';
